42a0a5d519e93da6d8db8e3f7506321a1f7cf826
turismofmca <planetaencorto7475@gmail.com>
2025-11-16 20:55:22 -0400

    avances

---
+++ INICIAR.bat
@@ -0,0 +1,7 @@
+orporation. Todos los derechos reservados.
+
+C:\Users\Angel>AIzaSyCTi-LOaJyzELaZ7YPdpByLDobVksnG-vk
+"AIzaSyCTi-LOaJyzELaZ7YPdpByLDobVksnG-vk" no se reconoce como un comando interno o externo,
+programa o archivo por lotes ejecutable.
+
+C:\Users\Angel>
