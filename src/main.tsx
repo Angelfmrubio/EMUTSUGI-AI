7d432da32e7c8cb9a2fc3b202d32d879a7337c0c
turismofmca <planetaencorto7475@gmail.com>
2025-11-23 01:18:40 -0400

    ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+/* 1. Estilos por defecto (Las pijamas) */
+import './index.css';
+import './estilos.css';
+
+/* 2. TU ESTILO REY (Al final para que gane y cubra todo de Azul) */
+import './styles/nexus-global.css'; 
 
 createRoot(document.getElementById("root")!).render(<App />);
